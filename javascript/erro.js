9d7518e9a081e1c3272d8cda5fb2aef8c39b1835
andreerfr <andreerci@gmail.com>
2025-06-13 12:16:07 -0300

    troca e ajusts

---
+++ javascript/erro.js
@@ -0,0 +1,17 @@
+function inicial() {
+
+    window.location.href = "pagina_inicial.html";
+
+};
+
+function recarregar() {
+
+    window.location.href = "erropadrao.html";
+
+};
+
+function sair() {
+
+    window.location.href = "pagina_login.html";
+
+};
