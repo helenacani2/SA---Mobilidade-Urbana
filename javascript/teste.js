90ac0fea1ff275fc6148708e62cde9ac688bc23b
andreerfr <andreerci@gmail.com>
2025-06-13 09:45:43 -0300

    Ultima versão

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,21 +1,3 @@
-function todos_os_usuarios() {
-
-    window.location.href = "";
-
-};
-
-function criar_usuario() {
-
-    window.location.href = "pagina_cadastro.html";
-
-};
-
-function informacoes_do_usuario() {
-
-    window.location.href = "";
-
-};
-
 document.addEventListener('DOMContentLoaded', function teste_hamburguer() {
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.querySelector('.menu-hamburguer');
@@ -29,27 +11,3 @@ document.addEventListener('DOMContentLoaded', function teste_hamburguer() {
         }
     });
 });
-
-function ADM_burger() {
-
-    var menu_ADM = document.getElementById("menu_burger_aparece");
-
-    if (menu_ADM.style.display == "none") {
-
-        menu_ADM.style.display = "block";
-
-        document.getElementById("ADM_burger").innerHTML = "🗙";
-
-        document.getElementById("ADM_burger").style.color = "black";
-
-    } else {
-
-        menu_ADM.style.display = "none";
-
-        document.getElementById("ADM_burger").innerHTML = "☰";
-
-        document.getElementById("ADM_burger").style.color = "white";
-
-    };
-
-};
