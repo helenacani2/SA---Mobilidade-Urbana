b060811133464fdb65943f6c748987df5b391bc8
helenacani2 <you@example.com>
2025-10-10 12:50:04 -0300

    Alterações

--- php/perfil_condutor.php
+++ php/perfil_condutor.php
@@ -60,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <?php
 
-            if ($_SESSION["cargo_funcionario"] == "Gerente" || "") {
+            if ($_SESSION["cargo_funcionario"] == "Gerente" || "Equipe_Atendimento") {
 
                 echo '
 
