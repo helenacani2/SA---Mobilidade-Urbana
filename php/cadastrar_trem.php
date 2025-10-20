5ca45e8c16ef813cebb1d165cf47c990a483fc7b
Iago-KK <iago_kleestadt@estudante.sesisenai.org.br>
2025-10-20 10:14:29 -0300

    Continuação do cadastro de trem

--- php/cadastrar_trem.php
+++ php/cadastrar_trem.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../midias/logomenor.png" type="icon">
+    <link rel="stylesheet" href="../css/cadastrar_trem.css?v=<?php echo time(); ?>">
     <title>Cadastrar novo trem</title>
 </head>
 
@@ -12,12 +13,25 @@
 
     <form action="cadastrar_trem_aux.php" method="POST">
 
-        <label for="TremNome">Nome do Trem</label>
+        <label for="TremNome" class="Legenda">Nome do Trem</label>
         
-        <input type="text" id="TremNome" name="TremNome" required>
+        <input type="text" id="TremNome" class="Texto" name="TremNome" required>
+
+        <br>
+
+        <label for="TremData" class="Legenda">Data de Fabricação do Trem</label>
+        
+        <input type="text" id="TremData"class="Texto" name="TremData" required>
+
+        <br>
+
+        <label for="TremFabricante" class="Legenda">Fabricante do Trem</label>
+        
+        <input type="text" id="TremFabricante"class="Texto" name="TremFabricante" required>
 
     </form>
 
 </body>
 
 </html>
+
